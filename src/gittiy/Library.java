05853f97a03418669c32fb0d53dac0204b8331a6
Furqan Buttar <asgharfur92@gmail.com>
2024-10-05 12:00:58 +0500

    Added removeBook function to Library class

--- src/gittiy/Library.java
+++ src/gittiy/Library.java
@@ -13,4 +13,13 @@ System.out.println(bookName + " added to library. ");
 
 public void listBooks() {
 System.out.println("Books in library: " + books);
-} }
+}
+public void removeBook(String bookName) {
+    if (books.remove(bookName)) {
+        System.out.println(bookName + " removed from library.");
+    } else {
+        System.out.println(bookName + " not found in library.");
+    }
+}
+
+}
